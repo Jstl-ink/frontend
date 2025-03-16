c207e36ef1a4974e4639d792bd01e92a3678a91f
paul <paul0802@icloud.com>
2025-03-16 21:09:24 +0100

    added sample request for user page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-export default function App() {
+import {Page, PageApi} from "./sdk";
+import {useEffect, useState} from "react";
+import '@mantine/core/styles.css';
+import {MantineProvider, Card, Image, Text} from '@mantine/core';
 
-  return (
-    <div className="text-blue-400 text-center">
-      Hi
-    </div>
-  )
+export default function App() {
+    const pageApi = new PageApi()
+    const [page, setPage] = useState({} as Page);
+
+    useEffect(() => {
+        pageApi.getPageById({pageId: "Arrgh!"}).then(value => setPage(value))
+    }, [])
+
+    console.log(page.img)
+
+    return (
+        <MantineProvider>
+            <main className="p-10 h-screen bg-gray-700">
+                <Card shadow="sm" padding="lg" radius="md" withBorder>
+                    <Card.Section component="a" href="https://mantine.dev/">
+                        <Image
+                            src={page.img}
+                            height={160}
+                            alt={page.id}
+                        />
+                    </Card.Section>
+
+                    <Text fw={500}>{page.name}</Text>
+
+                    <Text size="sm" c="dimmed">
+                        {page.bio}
+                    </Text>
+
+                    {page?.links?.map(link => (
+                        <a href={link.link} key={link.link}>
+                            {link.name}
+                        </a>
+                    ))}
+                </Card>
+            </main>
+        </MantineProvider>
+    )
 }
