d4faff7f616d0061f3532fa8cb8cffdfd2619e31
paul <paul0802@icloud.com>
2025-05-15 18:10:31 +0200

    made banner image optional

--- src/LinkTreePage.tsx
+++ src/LinkTreePage.tsx
@@ -8,11 +8,11 @@ interface LinkTreePageProps {
 export default function LinkTreePage({page}: LinkTreePageProps) {
     return (
         <div className="min-h-screen text-white flex flex-col items-center font-sans">
-            <img
+            {page?.img && <img
                 src={page?.img}
                 alt={page?.name}
                 className="mask-b-from-70% w-full h-60 object-top object-cover"
-            />
+            />}
             <div className="p-8 w-full flex flex-col items-center">
                 <h1 className="text-2xl font-bold">{page?.name}</h1>
                 <p className="text-gray-400 mb-8">{page?.bio}</p>
